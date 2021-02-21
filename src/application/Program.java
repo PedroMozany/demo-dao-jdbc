822f5b19ad2519ef08c459f8917d161d5d99f89d
unknown <pedro.mozanny@hotmail.com>
2021-02-21 00:51:52 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,10 +30,15 @@ public class Program {
 			System.out.println(obj);
 		}
 		System.out.println("\n=== test 4: Seller insert ===");
-        Seller newSeller = new Seller(null, "Greg", "greg@hotnail.com", new Date(), 4000.0, department);
-        sellerDao.insert(newSeller);
-        System.out.println("Inserted! New id = " + newSeller.getId());
-        
+		Seller newSeller = new Seller(null, "Greg", "greg@hotnail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+
+		System.out.println("\n=== test 5: Seller insert ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
